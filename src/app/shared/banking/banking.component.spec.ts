b2c7f2e2bc083f1d00ea0bd3339699e4b463ce51
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-05-17 16:29:46 -0300

    fazendo teste nos ifs

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -39,6 +39,13 @@ describe('BankingComponent', () => {
     expect(component.getPoupanca).toEqual(0);
     expect(component.getCarteira).toEqual(60);
   });
+  it(`
+    (U) setSacar(): should transfer poupanca dont have string (isNaN) or poupanca < value`, ()=>{
+      expect(component.setSacar('string')).not.toBeTruthy();
+      expect(component.setSacar('100')).not.toBeTruthy();
+      expect(component.getPoupanca).toEqual(10);
+      expect(component.getCarteira).toEqual(50);
+  });
 
   it(`
     (U) setDepositar(): should transfer carteira from poupanca`, ()=>{
@@ -47,4 +54,13 @@ describe('BankingComponent', () => {
     expect(component.getCarteira).toEqual(0);
     expect(component.getPoupanca).toEqual(60);
   });
+
+  it(`
+    (U) setDepositar(): should transfer poupanca dont have string (isNaN) or carteira < value`, ()=>{
+      expect(component.setDepositar('string')).not.toBeTruthy();
+      expect(component.setDepositar('100')).not.toBeTruthy();
+      expect(component.getPoupanca).toEqual(10);
+      expect(component.getCarteira).toEqual(50);
+  });
+  
 });
