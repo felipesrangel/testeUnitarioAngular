6f65ac8945187216c89aea68d3adb20082eb7a8f
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-05-17 16:19:45 -0300

    fazendo testes nas funcoes de get e set

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,31 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`
+    (U) getPoupanca(): should have poupanca = 10`, ()=>{
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it(`
+    (U) getCarteira(): should have carteira = 50`, ()=>{
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it(`
+    (U) setSacar(): should transfer poupanca from carteira`, ()=>{
+    component.setSacar("10");
+    fixture.detectChanges();
+    
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it(`
+    (U) setDepositar(): should transfer carteira from poupanca`, ()=>{
+    component.setDepositar("50");
+    
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
+  });
 });
